86528f16b77bf5f93b390d828fa82eebe66b281c
barbara <barbara@pc-barbara>
2017-02-02 20:27:49 +0100

    ejercicio 18 a medias, ejercicio 19 empezado

--- src/ejercicio18/Ej18Controller.java
+++ src/ejercicio18/Ej18Controller.java
@@ -45,6 +45,38 @@ public class Ej18Controller extends Controller{
 			e.printStackTrace();
 		}
 	}
+	protected void despedirseGet(HttpServletRequest request, HttpServletResponse response){
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			response.setContentType("text/html");
+
+			out.println("Ejercicio 18 TEMA 7 <br/><br/>" + "<strong>FORMULARIO SALUDAR NOMBRE</strong><br/><br/>");
+
+			out.println("<form action=\"\" method=\"post\" >");
+			out.println("<label for=\"idNombre\">Nombre: </label>" + "<input type=\"text\" name=\"nombre\" id=\"idNombre\"> <br/>");
+			out.println("<input type=\"submit\" value=\"Saludar\">");
+			out.println("</form>");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	protected void despedirsePost(HttpServletRequest request, HttpServletResponse response){
+		PrintWriter out;
+		String nombre = request.getParameter("nombre");
+		try {
+			out = response.getWriter();
+			response.setContentType("text/html");
+
+			out.println("Ejercicio 18 TEMA 7 <br/><br/>" + "<strong>DESPEDIR NOMBRE</strong><br/><br/>");
+			
+			out.println("Adiós "+nombre+" !!!<br/>");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 	
 		saludarGet(request, response);
