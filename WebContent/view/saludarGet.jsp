e6d94145ce9cd3057457e932f43eea52d34d67d8
barbara <barbara@pc-barbara>
2017-02-08 18:24:59 +0100

    Terminado ejercicio 20, queda el 21

---
+++ WebContent/view/saludarGet.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	Ejercicio 20 TEMA 7
+	<br />
+	<br />
+	<strong>FORMULARIO SALUDAR NOMBRE</strong>
+	<br />
+	<br />
+
+	<form action="<%= request.getAttribute("baseURL") %>t7/ej18/saludar" method="post">
+		<label for="idNombre">Nombre: </label> 
+		<input type="text" name="nombre" id="idNombre"> <br/> 
+		<input type="submit" value="Saludar">
+</body>
+</html>
--- src/ejercicio18/Ej18Controller.java
+++ src/ejercicio18/Ej18Controller.java
@@ -13,8 +13,11 @@ import org.mvc.Controller;
 @SuppressWarnings("serial")
 @WebServlet({ "/t7/ej18", "/t7/ej18/", "/t7/ej18/*" })
 public class Ej18Controller extends Controller {
-	public void saludarGet() throws IOException {
-		PrintWriter out;
+	public void saludarGet() throws IOException, ServletException {
+		
+		request.getRequestDispatcher("/view/saludarGet.jsp").forward(request, response);
+		
+		/*PrintWriter out;
 
 		out = response.getWriter();
 		response.setContentType("text/html");
@@ -25,12 +28,14 @@ public class Ej18Controller extends Controller {
 		out.println("<label for=\"idNombre\">Nombre: </label>"
 				+ "<input type=\"text\" name=\"nombre\" id=\"idNombre\"> <br/>");
 		out.println("<input type=\"submit\" value=\"Saludar\">");
-		out.println("</form>");
+		out.println("</form>");*/
 
 	}
 
-	public void saludarPost() throws IOException {
-		PrintWriter out;
+	public void saludarPost() throws IOException, ServletException {
+		request.setAttribute("nombre",request.getParameter("nombre"));
+		request.getRequestDispatcher("/view/saludarPost.jsp").forward(request, response);
+		/*PrintWriter out;
 		String nombre = request.getParameter("nombre");
 
 		out = response.getWriter();
@@ -38,28 +43,34 @@ public class Ej18Controller extends Controller {
 
 		out.println("Ejercicio 18 TEMA 7 <br/><br/>" + "<strong>SALUDAR NOMBRE</strong><br/><br/>");
 
-		out.println("Hola " + nombre + " !!!<br/>");
+		out.println("Hola " + nombre + " !!!<br/>");*/
 
 	}
 
-	protected void despedirseGet() throws IOException {
-		PrintWriter out;
+	public void despedirseGet() throws IOException, ServletException {
+		
+		request.getRequestDispatcher("/view/despedirseGet.jsp").forward(request, response);
+		
+		/*PrintWriter out;
 
 		out = response.getWriter();
 		response.setContentType("text/html");
 
-		out.println("Ejercicio 19 TEMA 7 <br/><br/>" + "<strong>FORMULARIO SALUDAR NOMBRE</strong><br/><br/>");
+		out.println("Ejercicio 19 TEMA 7 <br/><br/>" + "<strong>FORMULARIO DESPEDIR NOMBRE</strong><br/><br/>");
 
 		out.println("<form action=\""+this.baseURL+"t7/ej18/despedirse\" method=\"post\" >");
 		out.println("<label for=\"idNombre\">Nombre: </label>"
 				+ "<input type=\"text\" name=\"nombre\" id=\"idNombre\"> <br/>");
-		out.println("<input type=\"submit\" value=\"Saludar\">");
-		out.println("</form>");
+		out.println("<input type=\"submit\" value=\"Despedirse\">");
+		out.println("</form>");*/
 
 	}
 
-	protected void despedirsePost() throws IOException {
-		PrintWriter out;
+	public void despedirsePost() throws IOException, ServletException {
+		request.setAttribute("nombre",request.getParameter("nombre"));
+		request.getRequestDispatcher("/view/despedirsePost.jsp").forward(request, response);
+		
+		/*PrintWriter out;
 		String nombre = request.getParameter("nombre");
 
 		out = response.getWriter();
@@ -67,8 +78,12 @@ public class Ej18Controller extends Controller {
 
 		out.println("Ejercicio 19 TEMA 7 <br/><br/>" + "<strong>DESPEDIR NOMBRE</strong><br/><br/>");
 
-		out.println("Adiós " + nombre + " !!!<br/>");
+		out.println("Adiós " + nombre + " !!!<br/>");*/
 
+	}
+	
+	public void indexGet() throws IOException, ServletException{
+		saludarGet();
 	}
 	/*
 	 * protected void doGet(HttpServletRequest request, HttpServletResponse
